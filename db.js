10c050b0f4abe98260f5d7fac252c1a05035fd5e
Jai <jayachandran9629040900@gmail.com>
2023-10-27 20:51:36 +0530

    Notes Backend is Ready!

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-const connect=mongoose.connect("mongodb+srv://jai:jai@cluster0.2hpd1gu.mongodb.net/NoteTakingApp?retryWrites=true&w=majority")
+const connect=mongoose.connect(process.env.MongoURL)
 
 module.exports={
     connect
